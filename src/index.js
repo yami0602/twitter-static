6f002773b613ccf3a09381f2f52e0ac91772da81
Yami0602 <ysagales@techlaunch.io>
2018-02-27 20:57:12 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import avatar from "./batman-512.png";
 
-function TwitterTweet() {
+function Time() {
+  return <span className="time">3 hrs ago</span>;
+}
+
+function Message() {
+  return <div className="message">This is a message</div>;
+}
+
+function NameWithHandle() {
   return (
-    <div className="container">
-      <div className="avatar" />
-      <img src={avatar} width="150px" height="150px" />
+    <span className="name-with-handle">
+      <span className="handle">@handle A Name</span>
+      <span className="name" />
+    </span>
+  );
+}
 
-      <div className="info">
-        <div className="handle">
-          <div className="handle-name">@dusani</div>
-          <div className="elasped-time">. 3hrs Ago</div>
+function Avatar() {
+  return (
+    <img
+      src="https://cdn4.iconfinder.com/data/icons/superheroes/512/batman-512.png"
+      className="avatar"
+      alt="avatar"
+      width="50px"
+      height="50px"
+    />
+  );
+}
+
+const ReplyButton = () => <i className="fa fa-reply reply-button" />;
+
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
+
+const LikeButton = () => <i className="fa fa-heart heart-button" />;
+
+const MoreOptionButton = () => (
+  <i className="fa fa-ellipsis-h MoreOption-button" />
+);
+
+function Tweet() {
+  return (
+    <div className="tweet">
+      <Avatar />
+      <div className="content">
+        <NameWithHandle />
+        <Time />
+        <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionButton />
         </div>
-        <div className="message">
-          Etiam porta sem malesuada magna mollis euismod.
-        </div>
-        <div className="symbols">Testing</div>
       </div>
     </div>
   );
 }
 
-ReactDOM.render(<TwitterTweet />, document.getElementById("root"));
+ReactDOM.render(<Tweet />, document.getElementById("root"));
